8c1d824e73c826b5499a60ce054e77a10bc3a2fd
Vivek Deshmukh <vivekdeshmukh98@gmail.com>
2025-09-07 12:34:21 +0530

    feat: Refactor FilterContext and FilterBar components for improved readability and functionality

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ScrollToTop } from './components';
-import { BrowserRouter as Router} from 'react-router-dom';
-import {FilterProvider} from './context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ScrollToTop } from "./components";
+import { BrowserRouter as Router } from "react-router-dom";
+import { FilterProvider } from "./context";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
   <React.StrictMode>
     <Router>
       <FilterProvider>
-      <ScrollToTop />
-      <App />
+        <ScrollToTop />
+        <App />
       </FilterProvider>
     </Router>
   </React.StrictMode>
 );
-
