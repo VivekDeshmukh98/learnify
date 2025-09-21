cdc34df44f7e07d00ff99a1c02c44f242e45a5de
Vivek Deshmukh <vivekdeshmukh98@gmail.com>
2025-09-21 11:25:07 +0530

    feat: Add dashboard functionality with order retrieval and display, including DashboardCard and DashboardEmpty components

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import {  Route,Routes } from 'react-router-dom'
-import {HomePage} from '../pages/Home/HomePage'
-import { ProductsList ,ProductDetail} from '../pages'
-import {Login} from '../pages/index'
-import {Register} from '../pages/index'
-import {CartPage} from '../pages/Cart/CartPage'
-import { ProtectedRoutes } from './ProtectedRoutes'
-import { OrderPage } from '../pages/Order/OrderPage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../pages/Home/HomePage";
+import { ProductsList, ProductDetail, DashboardPage } from "../pages";
+import { Login } from "../pages/index";
+import { Register } from "../pages/index";
+import { CartPage } from "../pages/Cart/CartPage";
+import { ProtectedRoutes } from "./ProtectedRoutes";
+import { OrderPage } from "../pages/Order/OrderPage";
 
 export const AllRoutes = () => {
   // const token=true;
@@ -14,25 +14,37 @@ export const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="products" element={<ProductsList />} />
-      <Route path='products/:id' element={<ProductDetail />} />
+      <Route path="products/:id" element={<ProductDetail />} />
 
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      <Route path="/cart" element={
-        <ProtectedRoutes>
-        <CartPage />
-        </ProtectedRoutes>
-        } />
+      <Route
+        path="/cart"
+        element={
+          <ProtectedRoutes>
+            <CartPage />
+          </ProtectedRoutes>
+        }
+      />
 
-              <Route path="/order-summary" element={
-        <ProtectedRoutes>
-        <OrderPage />
-        </ProtectedRoutes>
-        } />
+      <Route
+        path="/order-summary"
+        element={
+          <ProtectedRoutes>
+            <OrderPage />
+          </ProtectedRoutes>
+        }
+      />
 
-      
- 
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoutes>
+            <DashboardPage />
+          </ProtectedRoutes>
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
