4b0c4aac25f9a66f1c48c2a0aa796da0754e5e61
Vivek Deshmukh <vivekdeshmukh98@gmail.com>
2025-10-02 14:47:05 +0530

    feat: Update placeholder text in Login component for improved clarity

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -49,7 +49,7 @@ export const Login = () => {
             </label>
             <input
               type="email"
-              placeholder="test@gmail.com"
+              placeholder="test@mail.com"
               id="email"
               name="email"
               required
@@ -69,7 +69,7 @@ export const Login = () => {
               type="password"
               id="password"
               name="password"
-              placeholder="abcdef"
+              placeholder="test1234"
               required
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm 
                          rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
